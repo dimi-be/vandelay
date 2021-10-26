3d457ed43cd8eaf2479dcfa75a6bd17984869610
Dimitri Michaux <dimitri.michaux@gmail.com>
2021-10-26 17:07:39 +0200

    open html file as dom document

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import path from "path";
+import { FileRepository } from "./repositories/filerepository"
+
+const rootDir = path.resolve(process.argv[2]);
+
+console.log('rootDir', rootDir);
+
+(async () => {
+    const index = path.join(rootDir, 'index.html');
+    const indexDoc = await FileRepository.OpenHtmlDocument(index);
+
+    console.log(indexDoc.getElementsByTagName('html')[0].innerHTML);
+})();

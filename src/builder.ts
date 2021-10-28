a0f7cdf9619852d2cace5604b1bbaf85b54043e6
Dimitri Michaux <dimitri.michaux@gmail.com>
2021-10-28 20:35:34 +0200

    test if contents of index.html is copied

--- src/builder.ts
+++ src/builder.ts
@@ -1,12 +1,19 @@
 import path from "path";
-import { writeFileSync } from "fs";
-import { FileRepository } from "./repositories/filerepository"
+import fs from "fs";
+import os from 'os';
+import { FileRepository } from "./repositories/filerepository";
 
 export class Builder {
     public async build(srcPath: string, targetPath: string){
         const index = path.join(srcPath, 'index.html');
         const indexDoc = await FileRepository.OpenHtmlDocument(index);
     
-        writeFileSync(`${targetPath}/index.html`, '');
+        const targetFilePath = path.join(targetPath, 'index.html');
+        const targetFile = fs.openSync(targetFilePath, 'w');
+
+        fs.writeSync(targetFile, '<!DOCTYPE html>');
+        fs.writeSync(targetFile, os.EOL);
+        fs.writeSync(targetFile, indexDoc.querySelector('html').outerHTML);
+        fs.closeSync(targetFile);
     }
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,17 +1,18 @@
 import { execSync } from 'child_process';
-import { mkdirSync, readdirSync } from "fs";
-import { resolve } from "path";
+import fs from "fs";
+import path from "path";
 import { assert } from "chai";
 
 import { Builder } from '../src/builder';
+import { JSDOM } from 'jsdom';
 
-const tmpFolderPath = resolve('tmp');
-const basicTestFiles = resolve('tests/test-files/basic/posts');
+const tmpFolderPath = path.resolve('tmp');
+const basicTestFiles = path.resolve('tests/test-files/basic');
 
 describe('integration: Builder', function() {
     this.beforeEach(function() {
         execSync(`rm -rf ${tmpFolderPath}`);
-        mkdirSync(tmpFolderPath);
+        fs.mkdirSync(tmpFolderPath);
     });
 
     describe('build', function() {
@@ -19,8 +20,20 @@ describe('integration: Builder', function() {
             const builder = new Builder();
             await builder.build(basicTestFiles, tmpFolderPath);
 
-            const files = readdirSync(tmpFolderPath);
+            const files = fs.readdirSync(tmpFolderPath);
             assert.include(files, 'index.html');
+        });
+
+        it('should build index.html', async function() {
+            const builder = new Builder();
+            await builder.build(basicTestFiles, tmpFolderPath);
+
+            const buffer = fs.readFileSync(path.join(tmpFolderPath, 'index.html'));
+            const document = new JSDOM(buffer).window.document;
+
+            assert.equal(document.title, 'Basic Test');
+            assert.equal(document.getElementById('header').childNodes[0].textContent.trim(), 'Basic Test');
+            assert.equal(document.getElementById('paragraph').childNodes[0].textContent.trim(), 'Basic content');
         });
     });
 });
